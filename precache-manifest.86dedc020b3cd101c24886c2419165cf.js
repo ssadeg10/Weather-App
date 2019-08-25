3313c004de4f6b595c66aac716af0ca7fa873215
ssadeg10 <wildhunter360@gmail.com>
2019-08-24 22:07:16 -0700

    Updates

---
+++ precache-manifest.86dedc020b3cd101c24886c2419165cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d54c22501c76de2a9b1482c0dd3a82",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "cad8ffd987de4e8c4ed3",
+    "url": "/Weather-App/static/css/main.7c63b36a.chunk.css"
+  },
+  {
+    "revision": "f5191efc4b65b5ec193a",
+    "url": "/Weather-App/static/js/2.f7bcb1a7.chunk.js"
+  },
+  {
+    "revision": "cad8ffd987de4e8c4ed3",
+    "url": "/Weather-App/static/js/main.93bdbee0.chunk.js"
+  },
+  {
+    "revision": "399d546ea401ac762887",
+    "url": "/Weather-App/static/js/runtime~main.4c9dffef.js"
+  }
+]);
